6635a70dcbabb0c81a16b0e9b3507d2c109b6114
blaz1 <arti.iluminati@gmail.com>
2015-11-28 00:37:43 +0100

    added physics, arts, english

--- app/src/main/java/eu/execom/hackaton/beacon/model/Section.java
+++ app/src/main/java/eu/execom/hackaton/beacon/model/Section.java
@@ -8,4 +8,20 @@ public class Section {
     private Integer idSection;
 
     private String name;
+
+    public Integer getIdSection() {
+        return idSection;
+    }
+
+    public void setIdSection(Integer idSection) {
+        this.idSection = idSection;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
